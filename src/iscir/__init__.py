f39262cbe07ae87e8ce92a3ef0c8614e4f7028fd
Panagiota Grosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-07-19 08:45:32 +0300

    Export FFT-based FMCW sensor

--- src/iscir/__init__.py
+++ src/iscir/__init__.py
@@ -1,7 +1,15 @@
 """ISCIR FMCW robotic obstacle-avoidance research package."""
 
 from .robotics.controller import ReactiveController
+from .sensing.fft_sensor import FFTFMCWSensor
 from .sensing.sensor import FMCWSensor
 from .simulation.models import Obstacle, RobotState, Scene
 
-__all__ = ["FMCWSensor", "Obstacle", "ReactiveController", "RobotState", "Scene"]
+__all__ = [
+    "FFTFMCWSensor",
+    "FMCWSensor",
+    "Obstacle",
+    "ReactiveController",
+    "RobotState",
+    "Scene",
+]
