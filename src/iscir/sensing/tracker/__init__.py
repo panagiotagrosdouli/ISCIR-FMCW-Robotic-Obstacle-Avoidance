c4d062669632e516c4617e9bcc7e5a08ab5608cb
Panagiota Grosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-07-19 09:16:19 +0300

    Export radar track lifecycle types

--- src/iscir/sensing/tracker/__init__.py
+++ src/iscir/sensing/tracker/__init__.py
@@ -1,5 +1,12 @@
 """Multi-target tracking primitives for radar detections."""
 
 from .kalman import ConstantVelocityKalmanFilter, KalmanFilterConfig
+from .track import Track, TrackLifecycleConfig, TrackStatus
 
-__all__ = ["ConstantVelocityKalmanFilter", "KalmanFilterConfig"]
+__all__ = [
+    "ConstantVelocityKalmanFilter",
+    "KalmanFilterConfig",
+    "Track",
+    "TrackLifecycleConfig",
+    "TrackStatus",
+]
